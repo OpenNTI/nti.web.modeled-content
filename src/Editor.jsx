d2de975356f3bf43303915e88c8127fb45f31e9b
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-05-24 13:12:43 -0500

    default file attachments to off for now

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -47,7 +47,7 @@ export default class Editor extends React.Component {
 
 
 	static defaultProps = {
-		allowInsertFile: true,
+		allowInsertFile: false,
 		allowInsertImage: true,
 		allowInsertVideo: false
 	}
