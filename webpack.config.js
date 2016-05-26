8df241058bfbb476c6029daf8e9b317e712d9dd5
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-05-26 13:51:57 -0500

    NTI-1129 - Make autoprefixer output prefixed rules for iOS 8

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ exports = module.exports = {
 
 
 	postcss: [
-		autoprefixer({ browsers: ['> 1%', 'last 2 versions'] })
+		autoprefixer({ browsers: ['> 1%', 'last 2 versions', 'iOS > 8'] })
 	],
 
 
