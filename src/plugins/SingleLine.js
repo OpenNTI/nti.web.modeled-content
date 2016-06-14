bf337a9821ef4969bcc7b4ec40a629d2fcc3e934
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-06-14 10:39:11 -0500

    Implement SingleLine plugins onPaste handler.
    Still need to figure out how to merge ContentBlocks into one ContentBlock. (preserving inline-styles and entities)

--- src/plugins/SingleLine.js
+++ src/plugins/SingleLine.js
@@ -1,4 +1,13 @@
 import Plugin from './Plugin';
+import {
+	ContentState,
+	// ContentBlock,
+	EditorState,
+	Modifier,
+	convertFromHTML
+} from 'draft-js';
+
+
 export default class SingleLine extends Plugin {
 
 	handlePastedText (text, html) {
@@ -6,6 +15,31 @@ export default class SingleLine extends Plugin {
 		// We need to take the (html || text) value and parse it into ContentBlocks and then merge
 		// them into one block (inserting spaces between)... then finally calling
 		// this.setEditorState with the new EditorState object.
+
+		const editorState = this.getEditorState();
+		const blocks = convertFromHTML(html || text);
+
+		if (blocks.length <= 1) { //let draft do its thing.
+			return;
+		}
+
+		//insert only the first block. TODO: figure out how to join blocks together.
+
+		// function join (a: ?ContentBlock, b: ContentBlock) {}
+		// const singleBlock = blocks.reduce((out, block) => join(out, block));
+
+		const newContent = Modifier.replaceWithFragment(
+			editorState.getCurrentContent(),
+			editorState.getSelection(),
+			ContentState.createFromBlockArray([blocks[0]]).getBlockMap()
+		);
+
+		this.setEditorState(EditorState.push(
+			editorState,
+			newContent,
+			'insert-fragment'
+		));
+
 		return true;
 	}
 
