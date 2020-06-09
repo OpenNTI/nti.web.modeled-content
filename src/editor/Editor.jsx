19f6ed5f8a6f9fe7e246e0c5083f64c3f995aef2
Andrew Ligon <andrew.ligon@nextthought.com>
2020-06-08 21:51:34 -0500

    take tagging strategies as a prop

--- src/editor/Editor.jsx
+++ src/editor/Editor.jsx
@@ -15,7 +15,7 @@ const cx = classnames.bind(Styles);
 const toDraftState = value => Parsers.HTML.toDraftState(value);
 const fromDraftState = draftState => Parsers.HTML.fromDraftState(draftState);
 
-const EditorPlugins = [
+const getEditorPlugins = () => ([
 	Plugins.LimitBlockTypes.create({allow: new Set([BLOCKS.ATOMIC, BLOCKS.UNSTYLED])}),
 	Plugins.LimitStyles.create({allow: new Set([STYLES.BOLD, STYLES.ITALIC, STYLES.UNDERLINE])}),
 	Plugins.ExternalLinks.create({allowedInBlockTypes: new Set([BLOCKS.UNSTYLED])}),
@@ -23,8 +23,9 @@ const EditorPlugins = [
 	Plugins.CustomBlocks.create({customRenderers: EditorCustomRenderers, customStyles: EditorCustomStyles}),
 	Plugins.KeepFocusInView.create(),
 	Plugins.ContiguousEntities.create()
-];
+]);
 
+ModeledContentEditor.Tagging = Plugins.Tagging;
 ModeledContentEditor.Buttons = Buttons;
 ModeledContentEditor.ContextProvider = ContextProvider;
 ModeledContentEditor.propTypes = {
@@ -36,13 +37,17 @@ ModeledContentEditor.propTypes = {
 		])
 	),
 
-	onContentChange: PropTypes.func
+	onContentChange: PropTypes.func,
+
+	taggingStrategies: PropTypes.array
 };
-export default function ModeledContentEditor ({className, content, onContentChange:onContentChangeProp, ...otherProps}) {
+export default function ModeledContentEditor ({className, content, onContentChange:onContentChangeProp, taggingStrategies, ...otherProps}) {
 	const editorContext = ContextProvider.useContext();
 
 	const contentRef = React.useRef(null);
 	const [editorState, setEditorState] = React.useState(null);
+	const [plugins, setPlugins] = React.useState(null);
+	const settingUp = !editorState || !plugins;
 
 	const addEditorRef = (editor) => {
 		editorContext?.setEditorInstance(editor);
@@ -54,6 +59,17 @@ export default function ModeledContentEditor ({className, content, onContentChan
 		}
 	}, [content]);
 
+	React.useEffect(() => {
+		if (taggingStrategies && taggingStrategies.length > 0) {
+			setPlugins([
+				Plugins.Tagging.create(taggingStrategies),
+				...getEditorPlugins()
+			]);
+		} else {
+			setPlugins(getEditorPlugins);
+		}
+	}, [taggingStrategies]);
+
 	const onContentChange = (newEditorState) => {
 		const newContent = fromDraftState(newEditorState);
 
@@ -63,11 +79,11 @@ export default function ModeledContentEditor ({className, content, onContentChan
 
 	return (
 		<div className={cx('mc-editor-wrapper', className)}>
-			{!editorState && (<Loading.Spinner />)}
-			{editorState && (
+			{settingUp && (<Loading.Spinner />)}
+			{!settingUp && (
 				<Editor
 					ref={addEditorRef}
-					plugins={EditorPlugins}
+					plugins={plugins}
 					className={cx('mc-editor')}
 					editorState={editorState}
 					onContentChange={onContentChange}
