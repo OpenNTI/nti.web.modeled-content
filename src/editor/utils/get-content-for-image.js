718c1dcfd453078d0a650c48f194ab8b6bb93bd5
Andrew Ligon <andrew.ligon@nextthought.com>
2020-07-13 19:04:15 -0500

    NTI-9491 add a utility for generating content with a whiteboard for an image

---
+++ src/editor/utils/get-content-for-image.js
@@ -0,0 +1,11 @@
+import {Utils} from '@nti/lib-whiteboard';
+
+export default async function getContentForImage (img) {
+	try {
+		const data = await Utils.createFromImage(img);
+
+		return data;
+	} catch (e) {
+		return null;
+	}
+}
