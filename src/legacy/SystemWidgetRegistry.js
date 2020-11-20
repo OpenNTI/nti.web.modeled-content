f59b16c0a749e3830cca08722d83f590aaec4614
Jonathan Grimes <jsg2021@gmail.com>
2020-11-20 13:14:31 -0600

    NTI-9858 - add link previews to "legacy" panel

--- src/legacy/SystemWidgetRegistry.js
+++ src/legacy/SystemWidgetRegistry.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Video from '@nti/web-video';
 import {Panel as Whiteboard} from '@nti/web-whiteboard';
 
+import * as LinkPreview from '../attachments/link-preview';
+
 import {FileAttachmentIcon} from './editor-parts';
 
 export default {
 	'application/vnd.nextthought.embeddedvideo': renderVideoWidget,
 	'application/vnd.nextthought.canvas': renderWhiteboardWidget,
-	'application/vnd.nextthought.contentfile': renderFileAttachment
+	'application/vnd.nextthought.contentfile': renderFileAttachment,
+	[LinkPreview.Handles]: renderLinkPreview,
 };
 
 
@@ -27,4 +30,10 @@ function renderWhiteboardWidget (_, properties) {
 function renderFileAttachment (_, properties) {
 	const {widget, id} = properties;
 	return React.createElement(FileAttachmentIcon, {data: widget, id});
+}
+
+function renderLinkPreview (_, properties) {
+	const {widget, id} = properties;
+	//LinkPreview.View
+	return React.createElement(LinkPreview.View, {attachment: widget, id});
 }
